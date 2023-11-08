fae9a14f6f731d506d59e1f469a7a8ecff7520cf
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-08 08:08:48 +0200

    Update App.jsx

--- react-maximillian/react-esentials/01-starting-project/src/App.jsx
+++ react-maximillian/react-esentials/01-starting-project/src/App.jsx
@@ -1,4 +1,12 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img
@@ -7,7 +15,7 @@ function Header() {
       />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
