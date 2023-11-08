eed3afeca63e294979046d0c8b4eb989fb47144c
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-08 11:21:37 +0200

    updated component

--- react-maximillian/react-esentials/01-starting-project/src/App.jsx
+++ react-maximillian/react-esentials/01-starting-project/src/App.jsx
@@ -1,7 +1,23 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img
+        src={image}
+        alt={title}
+      />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header() {
@@ -10,7 +26,7 @@ function Header() {
   return (
     <header>
       <img
-        src='src/assets/react-core-concepts.png'
+        src={reactImg}
         alt='Stylized atom'
       />
       <h1>React Essentials</h1>
@@ -27,10 +43,42 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            {CORE_CONCEPTS.map((prop) => (
+              <CoreConcept {...prop} />
+            ))}
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
+/*
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+*/
 
 export default App;
