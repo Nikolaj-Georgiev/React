ad4cff7b8ec8e34142119a711ccdc85a8f9dcf46
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-08 12:02:05 +0200

    updated file structure

--- react-maximillian/react-esentials/01-starting-project/src/App.jsx
+++ react-maximillian/react-esentials/01-starting-project/src/App.jsx
@@ -1,42 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img
-        src={image}
-        alt={title}
-      />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img
-        src={reactImg}
-        alt='Stylized atom'
-      />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
