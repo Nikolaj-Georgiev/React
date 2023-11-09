e877b872b3fbb2afc2c85f29d516312f50c96592
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-09 10:32:15 +0200

    updated components

--- react-maximillian/react-esentials/01-starting-project/src/App.jsx
+++ react-maximillian/react-esentials/01-starting-project/src/App.jsx
@@ -1,8 +1,17 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react';
+import { CORE_CONCEPTS } from './data1.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
+import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
+  // const stateArray = useState('Please click a button');//useState() ALWAYS returns an array with EXACTLY TWO ELEMENTS!!!
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic); // when setSelectedTopic is executed it schedule App() to be called again, but before that the selectedTopic is with the old value, no mather that in the UI the selectedTopic is updated and with the new value! Just to keep in mind.
+  }
+
   return (
     <div>
       <Header />
@@ -11,9 +20,24 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             {CORE_CONCEPTS.map((prop) => (
-              <CoreConcept {...prop} />
+              <CoreConcept
+                key={prop.id}
+                {...prop}
+              />
             ))}
           </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
