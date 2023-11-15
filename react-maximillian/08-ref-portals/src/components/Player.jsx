f64eee165c71565102076016b2d6cba896d93d38
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-15 23:05:56 +0200

    refs and portals

--- react-maximillian/08-ref-portals/src/components/Player.jsx
+++ react-maximillian/08-ref-portals/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState, useRef } from 'react';
+
 export default function Player() {
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+    // playerName.current.value = ''; you can empty the field like this but it's generally not a good practice since you manipulate the DOM and have an imperative code, not declarative!!!
+    playerName.current.value = '';
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerName}
+          type='text'
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
