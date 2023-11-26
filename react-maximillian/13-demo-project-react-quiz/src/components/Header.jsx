46b24038c6226417746663a4e8b3f80657e47676
NGNikolay <georgiev.nikolaj@gmail.com>
2023-11-26 18:34:14 +0200

    added header

---
+++ react-maximillian/13-demo-project-react-quiz/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logoImg from '../assets/quiz-logo.png';
+
+export default function Header() {
+  return (
+    <header>
+      <img
+        src={logoImg}
+        alt='Quiz logo'
+      />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
