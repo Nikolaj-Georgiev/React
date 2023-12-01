aa8ea42fa043c8063833ea8ab93084b25330e076
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-01 22:04:20 +0200

    finished the app

--- react-maximillian/13-demo-project-react-quiz/src/components/Summery.jsx
+++ react-maximillian/13-demo-project-react-quiz/src/components/Summery.jsx
@@ -2,6 +2,19 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 export default function Summery({ userAnswers }) {
+  const skippedAnswers = userAnswers.filter((answer) => answer === null);
+  const correctAnswers = userAnswers.filter(
+    (answer, index) => answer === QUESTIONS[index].answers[0]
+  );
+
+  const skippedAnswersShare = Math.round(
+    (skippedAnswers.length / userAnswers.length) * 100
+  );
+  const correctAnswersShare = Math.round(
+    (correctAnswers.length / userAnswers.length) * 100
+  );
+  const wrongAnswersShare = 100 - skippedAnswersShare - correctAnswersShare;
+
   return (
     <div id='summary'>
       <img
@@ -11,25 +24,43 @@ export default function Summery({ userAnswers }) {
       <h2>Quiz Completed!</h2>
       <div id='summary-stats'>
         <p>
-          <span className='number'>10%</span>
+          <span className='number'>{skippedAnswersShare}%</span>
           <span className='text'>Skipped</span>
         </p>
         <p>
-          <span className='number'>10%</span>
+          <span className='number'>{correctAnswersShare}%</span>
           <span className='text'>Answered correctly</span>
         </p>
         <p>
-          <span className='number'>10%</span>
+          <span className='number'>{wrongAnswersShare}%</span>
           <span className='text'>Answered incorrectly</span>
         </p>
       </div>
       <ol>
         {userAnswers.map((answer, index) => {
+          let cssClass = 'user-answer';
+
+          if (answer === null) {
+            cssClass += ' skipped';
+          } else if (answer === QUESTIONS[index].answers[0]) {
+            cssClass += ' correct';
+          } else {
+            cssClass += ' wrong';
+          }
+
           return (
-            <li key={answer}>
+            // typically using the index is incorrect!!!
+            <li key={index}>
               <h3>{index + 1}</h3>
               <p className='question'>{QUESTIONS[index].text}</p>
-              <p className='user-answer'>{answer}</p>
+              <p className={cssClass}>
+                {`Your answer: ${answer ?? 'Skipped'}`}
+              </p>
+              {cssClass.includes('wrong') && (
+                <p className={cssClass.replace('wrong', 'correct')}>
+                  {`Correct answer: ${QUESTIONS[index].answers[0]}`}
+                </p>
+              )}
             </li>
           );
         })}
