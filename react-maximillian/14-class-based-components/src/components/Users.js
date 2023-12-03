8d60029843ef1b6a0496caf243961afe2b0615ce
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-03 11:51:56 +0200

    transformed two components to class components

--- react-maximillian/14-class-based-components/src/components/Users.js
+++ react-maximillian/14-class-based-components/src/components/Users.js
@@ -1,16 +1,53 @@
-import { useState } from 'react';
+import { Component } from 'react';
+// import {  useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 
+
+class Users extends Component {
+  constructor () {
+    super();
+    this.state = {
+      showUsers: true,
+      more: 'Test'
+    };//in class components state ALWAYS is an OBJECT!!! and also has to be property named 'state'! -> N.B.
+  }
+  toggleUsersHandler() {
+    // this.state.showUsers = !this.state.showUsers; this is NOT how you do it!!! instead you call a special method this.setState()
+    // this.setState({showUsers: false}) // always pass an object { } behind the scenes React MERGES the ne object with the old one, DOES NOT OVERRIDE IT!!!
+    // or you can pass a function:
+    this.setState((curState) => {
+      return {
+        showUsers: !curState.showUsers
+      }
+    })
+  }
+  render() {
+
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/*
 const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
+  const [showUsers, setShowUsers] = useState(true);// REACT OVERRIDE THE OLD STATE WITH THE NEW STATE WHEN setShowUsers() is called!!! /or any setState() function/ 
 
   const toggleUsersHandler = () => {
     setShowUsers((curState) => !curState);
@@ -33,5 +70,5 @@ const Users = () => {
     </div>
   );
 };
-
+*/
 export default Users;
