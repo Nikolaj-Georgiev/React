35abee16a225eb30ea0e550580279b05d7532eef
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-04 14:33:18 +0200

    added error handling

--- react-maximillian/15-HTTP-requests/src/components/AvailablePlaces.jsx
+++ react-maximillian/15-HTTP-requests/src/components/AvailablePlaces.jsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+  //usually when handling errors we have these ⬆️ 3 states working together
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error('Failed fetching places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places, please try again later!',
+        });
+      }
+
       setIsFetching(false);
     }
     fetchPlaces();
@@ -23,6 +39,15 @@ export default function AvailablePlaces({ onSelectPlace }) {
     //     setAvailablePlaces(resData.places);
     //   });
   }, []);
+
+  if (error) {
+    return (
+      <Error
+        title='An error ocurred'
+        message={error.message}
+      />
+    );
+  }
 
   return (
     <Places
