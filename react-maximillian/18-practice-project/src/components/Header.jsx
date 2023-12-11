6eb7d90d489574ee5d10b9252a3384dbdafc2b79
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-11 20:45:52 +0200

    added addToCart functionality

--- react-maximillian/18-practice-project/src/components/Header.jsx
+++ react-maximillian/18-practice-project/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
-import Button from './UI/Button';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items?.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id='main-header'>
       <div id='title'>
@@ -13,7 +20,7 @@ export default function Header() {
       </div>
       <nav>
         {/* textOnly={true} => in React, just adding the textOnly automatically sets it to true */}
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
--- react-maximillian/18-practice-project/src/store/CartContext.jsx
+++ react-maximillian/18-practice-project/src/store/CartContext.jsx
@@ -9,19 +9,19 @@ const CartContext = createContext({
 function cartReducer(state, action) {
   if (action.type === 'ADD_ITEM') {
     //update the state to add a meal item...
-    const existingCartItemsIndex = state.items.findIndex(
+    const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
 
     const updatedItems = [...state.items];
 
-    if (existingCartItemsIndex > -1) {
-      const existingItem = state.items[existingCartItemsIndex];
+    if (existingCartItemIndex > -1) {
+      const existingItem = state.items[existingCartItemIndex];
       const updatedItem = {
         ...existingItem,
         quantity: existingItem.quantity + 1,
       };
-      updatedItems[existingCartItemsIndex] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     } else {
       updatedItems.push({ ...action.item, quantity: 1 });
     }
@@ -31,21 +31,22 @@ function cartReducer(state, action) {
 
   if (action.type === 'REMOVE_ITEM') {
     // remove item from the state
-    const existingCartItemsIndex = state.items.findIndex(
+    const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
 
-    const existingCartItem = state.items[existingCartItemsIndex];
+    const existingCartItem = state.items[existingCartItemIndex];
 
     const updatedItems = [...state.items];
+
     if (existingCartItem.quantity === 1) {
-      updatedItems.splice(existingCartItemsIndex, 1);
+      updatedItems.splice(existingCartItemIndex, 1);
     } else {
       const updatedItem = {
         ...existingCartItem,
         quantity: existingCartItem.quantity - 1,
       };
-      updatedItems[existingCartItemsIndex] = updatedItem;
+      updatedItems[existingCartItemIndex] = updatedItem;
     }
 
     return { ...state, items: updatedItems };
@@ -55,7 +56,7 @@ function cartReducer(state, action) {
 } // state and action are passed automatically by React! The goal of this fn is to return updated state.
 
 export function CartContextProvider({ children }) {
-  const [cart, dispatchCartAction] = useReducer(createContext, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
   function addItem(item) {
     dispatchCartAction({ type: 'ADD_ITEM', item });
@@ -70,6 +71,8 @@ export function CartContextProvider({ children }) {
     addItem,
     removeItem,
   };
+
+  console.log(cartContext);
 
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
