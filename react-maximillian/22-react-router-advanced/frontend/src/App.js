43e27c34c213d084fbb04e1ee037a91f85d29d4f
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-22 15:54:59 +0200

    added loader in the router

--- react-maximillian/22-react-router-advanced/frontend/src/App.js
+++ react-maximillian/22-react-router-advanced/frontend/src/App.js
@@ -37,7 +37,18 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       {
         path: 'events', element: <EventsRootLayout />, children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true, element: <EventsPage />, loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+                // later to be made
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            }
+          },
           { path: ':eventId', element: <EventDetailsPage /> },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> },
--- react-maximillian/22-react-router-advanced/frontend/src/pages/Events.js
+++ react-maximillian/22-react-router-advanced/frontend/src/pages/Events.js
@@ -1,28 +1,30 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
-const DUMMY_EVENTS = [
-  {
-    id: 'e1',
-    title: 'Some event'
-  },
-  {
-    id: 'e2',
-    title: 'Some other event'
-  },
-  {
-    id: 'e3',
-    title: 'Some third event'
-  }
-];
-
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  return <>
-    <h1>Events Page</h1>
-    <ul>
-      {DUMMY_EVENTS.map((event) => <li key={event.id}><Link to={event.id}>{event.title}</Link></li>)}
-    </ul>
-  </>
-};
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+    </>
+  );
+}
 
 export default EventsPage;
