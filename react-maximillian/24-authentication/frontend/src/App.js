db49c20b5edc373ffc59a92163994f6a2aee2751
NGNikolay <georgiev.nikolaj@gmail.com>
2023-12-30 16:55:57 +0200

    added authentication button

--- react-maximillian/24-authentication/frontend/src/App.js
+++ react-maximillian/24-authentication/frontend/src/App.js
@@ -13,6 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import AuthenticationPage, { action as authAction } from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
@@ -53,6 +54,11 @@ const router = createBrowserRouter([
             action: manipulateEventAction,
           },
         ],
+      },
+      {
+        path: 'auth',
+        element: <AuthenticationPage />,
+        action: authAction,
       },
       {
         path: 'newsletter',
--- react-maximillian/24-authentication/frontend/src/components/AuthFormOldWithState.js
+++ react-maximillian/24-authentication/frontend/src/components/AuthFormOldWithState.js
@@ -1,11 +1,14 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [searchParams] = useSearchParams();
-  const isLogin = searchParams.get('mode') === 'login';
+  const [isLogin, setIsLogin] = useState(true);
 
+  function switchAuthHandler() {
+    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
+  }
 
   return (
     <>
@@ -20,9 +23,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
+          <button onClick={switchAuthHandler} type="button">
             {isLogin ? 'Create new user' : 'Login'}
-          </Link>
+          </button>
           <button>Save</button>
         </div>
       </Form>
