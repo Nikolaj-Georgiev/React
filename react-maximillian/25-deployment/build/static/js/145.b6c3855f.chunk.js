5e215894977bde699de78779fa717240f051aa9c
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-01 12:50:09 +0200

    finished firebase deployment

---
+++ react-maximillian/25-deployment/build/static/js/145.b6c3855f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_temp=self.webpackChunkreact_temp||[]).push([[145],{145:(t,s,e)=>{e.r(s),e.d(s,{default:()=>l,loader:()=>a});var c=e(689),n=e(87);const r="PostList_list__OZCNc";var o=e(184);const i=function(t){let{posts:s}=t;return(0,o.jsx)("ul",{className:r,children:s.map((t=>(0,o.jsx)("li",{children:(0,o.jsx)(n.rU,{to:t.id.toString(),children:t.title})},t.id)))})};const l=function(){const t=(0,c.f_)();return(0,o.jsx)(i,{posts:t})};function a(){return fetch("https://jsonplaceholder.typicode.com/posts")}}}]);
+//# sourceMappingURL=145.b6c3855f.chunk.js.map
