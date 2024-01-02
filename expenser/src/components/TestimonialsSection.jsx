07d50868a9afbd865d76fc808a5336163cced6cf
NGNikolay <georgiev.nikolaj@gmail.com>
2024-01-02 16:47:50 +0200

    started Tastack Query project

---
+++ expenser/src/components/TestimonialsSection.jsx
@@ -0,0 +1,14 @@
+export default function TestimonialsPage() {
+  return (
+    <>
+      <h2>Some Nice Heading</h2>
+      <img
+        src=''
+        alt=''
+      />
+      <div>
+        <blockquote></blockquote>
+      </div>
+    </>
+  );
+}
